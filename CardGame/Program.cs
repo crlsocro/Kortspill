dd6b391f37d5f0c8eebe51101302bb6c1496adab
Håkon Hagen <haakonhagen@live.no>
2020-11-05 02:58:56 +0100

    Starting the coding

---
+++ CardGame/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CardGame
+{
+    class Program
+    {
+        private int amountOfPlayers;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many total players do you want? (2-4)");
+             int amountOfPlayers = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(amountOfPlayers);
+        }
+    }
+}
