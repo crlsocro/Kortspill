0701325d49d33f8e5b6a8d09373492b1d94d916c
Håkon Hagen <haakonhagen@live.no>
2020-11-16 22:33:27 +0100

    small text change

--- CardGame/TestFile.cs
+++ CardGame/TestFile.cs
@@ -12,9 +12,10 @@ namespace CardGame
             deck.createDeck();
 
             Card card = new Card((Card.Suit)0, (Card.Rank)0, (Card.Specialty)0);
-            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+            Assert.AreEqual(deck.theDeck[0], card);
         }
     }
 }
 
 //Hold over TestClass å bruk sann "tips" greier til å installere test frameworket
+//https://docs.microsoft.com/en-us/visualstudio/test/walkthrough-creating-and-running-unit-tests-for-managed-code?view=vs-2019
