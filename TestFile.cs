2479ba1fee315a50cc067f24be5bd645b48c05bc
Håkon Hagen <haakonhagen@live.no>
2020-11-16 22:25:08 +0100

    Added a test

---
+++ TestFile.cs
@@ -0,0 +1,16 @@
+namespace CardGame
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void TestIfDeckIsCreatedCorrectly()
+        {
+            Deck deck = new Deck();
+            deck.createDeck();
+
+            Card card = ;
+
+        }
+    }
+}
