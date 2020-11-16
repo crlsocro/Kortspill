7bad4dc7fff5d21e4ba683090cc315b40916cc0e
hkonn <hkonnhaagaas@gmail.com>
2020-11-16 23:28:15 +0100

    adding tests

--- CardGame/CardGameTest/UnitTest1.cs
+++ CardGame/CardGameTest/UnitTest1.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CardGame;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
 
 namespace CardGameTest
 {
@@ -12,6 +15,23 @@ namespace CardGameTest
             deck.createDeck();
 
             Card card = new Card((Card.Suit)0, (Card.Rank)0, (Card.Specialty)0);
+            Assert.AreEqual(deck.theDeck[0], card);
+        }
+
+        [TestMethod]
+        public void TestIfTossingRightCard()
+        {
+            Card c1 = new Card(Card.Suit.Clubs, Card.Rank.Ace, Card.Specialty.None);
+            Card c2 = new Card(Card.Suit.Diamonds, Card.Rank.Two, Card.Specialty.None);
+            Card c3 = new Card(Card.Suit.Clubs, Card.Rank.Three, Card.Specialty.None);
+            Card c4 = new Card(Card.Suit.Clubs, Card.Rank.Four, Card.Specialty.None);
+
+            Player player = new Player();
+            player.Hand.Add(c1);
+            player.Hand.Add(c2);
+            player.Hand.Add(c3);
+            player.Hand.Add(c4);
+
             Assert.AreEqual(deck.theDeck[0], card);
         }
     }
